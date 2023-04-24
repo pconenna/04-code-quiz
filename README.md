18256cfdc95185096894e2fe4a99963143538432
Peter Conenna <pconenna@depaul.edu>
2023-04-24 15:55:01 -0500

    changes made during tutoring session details are in README

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # 04-code-quiz
 ## Description
 This will be a code quiz. 
+
+
+## Credits 
+I attended a tutoring session with Vinnie Lopez. 
+Changes made:
+*using an array of objects instead of an object of objects to store the questions
+*attaching the checkAnswer function to button event handlers
+*changing the stucture of the nextQuestion and checkAnswer functions
+
--- script.js
+++ script.js
@@ -7,6 +7,28 @@ var option2 = document.querySelector("#option2");
 var option3 = document.querySelector("#option3");
 var option4 = document.querySelector("#option4");
 
+var arr = [{
+    question: "Which one of these is not a real programming language?",
+    choice1: "JavaScript",
+    choice2: "Python",
+    choice3: "Bungerscript",
+    choice4: "C#",
+    rightAnswer: "Bungerscript"
+}, {
+    question: "What language was Minecraft originally written in?",
+    choice1: "Python",
+    choice2: "Java",
+    choice3: "Kotlin",
+    choice4: "C#",
+    rightAnswer: "Java"
+}, {
+    question: "What is a variable?",
+    choice1: "A container to store data",
+    choice2: "something",
+    choice3: "something else",
+    choice4: "another wrong answer",
+    rightAnswer: "A container to store data"
+}]
 
 var quiz = {
     question1: {
@@ -35,44 +57,50 @@ var quiz = {
     }
 
 }
-var currentQuestion = quiz.question1;
+
 var questionIndex = 0;
 //starts quiz by hiding the intro and showing a question
 btnStart.addEventListener("click",function(event){
     introSection.setAttribute("class","sectionHide");
     answersSection.setAttribute("class","sectionShow")
-    nextQuestion(event);
-})
-option1.addEventListener("click",function(event){
-    nextQuestion(event);
-})
-option2.addEventListener("click",function(event){
-    nextQuestion(event);
-})
-option3.addEventListener("click",function(event){
-    nextQuestion(event);
-})
-option4.addEventListener("click",function(event){
-    nextQuestion(event);
+    nextQuestion();
 })
 
 
 
- function nextQuestion(event){
-    var target = event.target;
-    console.log(target);
-    var quizKeys = Object.keys(quiz);
-    currentQuestion = quiz[quizKeys[questionIndex]];
+ function nextQuestion(){
+     
+     //  var quizKeys = Object.keys(quiz);
+    //  var currentQuestion = quiz[quizKeys[questionIndex]];
+    var currentQuestion = arr[questionIndex]
     questionH2.innerHTML = currentQuestion.question;
     option1.textContent = currentQuestion.choice1;
     option2.textContent = currentQuestion.choice2;
     option3.textContent = currentQuestion.choice3;
     option4.textContent = currentQuestion.choice4;
+    option1.setAttribute('value', currentQuestion.choice1)
     //checkAnswer(target)
-    questionIndex++;
- }
+    ; //move this 
+}
 
- function checkAnswer(){
-    var questionKeys = Object.keys(currentQuestion);
-    var rightAnswer = currentQuestion.rightAnswer;
+function checkAnswer(event){
+    // var questionKeys = Object.keys(currentQuestion);
+    // var rightAnswer = currentQuestion.rightAnswer;
+    var target = event.target;
+    console.log(target.value);
+
+
+    questionIndex++
+    //check if any questions left
+    // if(questionIndex !==7){
+
+    // }else{
+
+        nextQuestion();
+    // }
  }
+ //dynamic building
+ option1.addEventListener("click",checkAnswer)
+ option2.addEventListener("click",checkAnswer)
+ option3.addEventListener("click",checkAnswer)
+ option4.addEventListener("click",checkAnswer)
