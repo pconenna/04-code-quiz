9908b17fbfce7ec48a597cee46f1470a98f5e629
Peter Conenna <pconenna@depaul.edu>
2023-04-26 16:12:57 -0500

    refactoring, decided to use an array to hold questions and used attributes to display questions

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ var option1 = document.querySelector("#option1");
 var option2 = document.querySelector("#option2");
 var option3 = document.querySelector("#option3");
 var option4 = document.querySelector("#option4");
+var score = 0;
+var highScore = 0;
 
 var arr = [{
     question: "Which one of these is not a real programming language?",
@@ -30,33 +32,6 @@ var arr = [{
     rightAnswer: "A container to store data"
 }]
 
-var quiz = {
-    question1: {
-        question: "Which one of these is not a real programming language?",
-        choice1: "JavaScript",
-        choice2: "Python",
-        choice3: "Bungerscript",
-        choice4: "C#",
-        rightAnswer: 3
-    },
-    question2: {
-        question: "What language was Minecraft originally written in?",
-        choice1: "Python",
-        choice2: "Java",
-        choice3: "Kotlin",
-        choice4: "C#",
-        rightAnswer: 2
-    },
-    question3: {
-        question: "What is a variable?",
-        choice1: "A container to store data",
-        choice2: "something",
-        choice3: "something else",
-        choice4: "another wrong answer",
-        rightAnswer: 1
-    }
-
-}
 
 var questionIndex = 0;
 //starts quiz by hiding the intro and showing a question
@@ -69,23 +44,23 @@ btnStart.addEventListener("click",function(event){
 
 
  function nextQuestion(){
-     
-     //  var quizKeys = Object.keys(quiz);
-    //  var currentQuestion = quiz[quizKeys[questionIndex]];
     var currentQuestion = arr[questionIndex]
     questionH2.innerHTML = currentQuestion.question;
-    option1.textContent = currentQuestion.choice1;
-    option2.textContent = currentQuestion.choice2;
-    option3.textContent = currentQuestion.choice3;
-    option4.textContent = currentQuestion.choice4;
-    option1.setAttribute('value', currentQuestion.choice1)
-    //checkAnswer(target)
-    ; //move this 
+   
+    option1.setAttribute('value', currentQuestion.choice1);
+    option1.textContent = option1.getAttribute('value');
+
+    option2.setAttribute('value', currentQuestion.choice2);
+    option2.textContent = option2.getAttribute('value'); 
+
+    option3.setAttribute('value', currentQuestion.choice3);
+    option3.textContent = option3.getAttribute('value');
+    
+    option4.setAttribute('value', currentQuestion.choice4);
+    option4.textContent = option4.getAttribute('value'); 
 }
 
 function checkAnswer(event){
-    // var questionKeys = Object.keys(currentQuestion);
-    // var rightAnswer = currentQuestion.rightAnswer;
     var target = event.target;
     console.log(target.value);
 
@@ -99,7 +74,7 @@ function checkAnswer(event){
         nextQuestion();
     // }
  }
- //dynamic building
+
  option1.addEventListener("click",checkAnswer)
  option2.addEventListener("click",checkAnswer)
  option3.addEventListener("click",checkAnswer)
