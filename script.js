0c0185ee79a87a34450f4455306d0f26cd69bd6b
Peter Conenna <pconenna@depaul.edu>
2023-04-27 15:59:20 -0500

    added more questions, added function to restart quiz and a broken function that attempts to clear scores

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ var secondsLeft = 60;
 var initialsInput = document.querySelector("#initialsInput");
 var submitBtn = document.querySelector("#submit");
 var leaderboard = document.querySelector("#leaderboard")
-
+var restartBtn = document.querySelector("#restartBtn");
+var clearBtn = document.querySelector("#clearBtn");
 var score = 0;
 var scores = JSON.parse(localStorage.getItem("scores")) ;
 if (scores == null){
@@ -23,12 +24,12 @@ if (scores == null){
 }
 
 var quiz = [{
-    question: "Which one of these is not a real programming language?",
+    question: "Which one of these is used for databse programming?",
     choice1: "JavaScript",
     choice2: "Python",
-    choice3: "Bungerscript",
+    choice3: "SQL",
     choice4: "C#",
-    rightAnswer: "Bungerscript"
+    rightAnswer: "SQL"
 }, {
     question: "What language was Minecraft originally written in?",
     choice1: "Python",
@@ -50,6 +51,48 @@ var quiz = [{
     choice3: "object",
     choice4: "boolean",
     rightAnswer: "object"
+},{
+    question: "What encloses the conditional in an if statement?",
+    choice1: "square brackets",
+    choice2: "parenthesis",
+    choice3: "curly braces",
+    choice4: "nothing",
+    rightAnswer: "parenthesis"
+},{
+    question: "Which loop is used to iterate of Objects?",
+    choice1: "for loop",
+    choice2: "while loop",
+    choice3: "for each loop",
+    choice4: "for in loop",
+    rightAnswer: "for in loop"
+},{
+    question: "Which CSS selector denotes a class named sectionHide?",
+    choice1: ".sectionHide",
+    choice2: "#sectionHide",
+    choice3: "sectionHide",
+    choice4: "none of the above",
+    rightAnswer: ".sectionHide"
+},{
+    question: "What does the ++ operator do?",
+    choice1: "Increments the value it is attached to.",
+    choice2: "Double addition.",
+    choice3: "Multiplication",
+    choice4: "The ++ is not an operator.",
+    rightAnswer: "Increments the value it is attached to."
+},{
+    question: "Which array method removes the last element from an array?",
+    choice1: "push",
+    choice2: "pop",
+    choice3: "shift",
+    choice4: "splice",
+    rightAnswer: "pop"
+},{
+    question: "What does the value 100% set in the statement flex 1 0 100%?",
+    choice1: "width",
+    choice2: "height",
+    choice3: "flex basis",
+    choice4: "size",
+    rightAnswer: "size"
 }]
 
 
@@ -59,6 +102,8 @@ btnStart.addEventListener("click",function(event){
     introSection.setAttribute("class","sectionHide");
     countDown();
     answersSection.setAttribute("class","sectionShow");
+    timerH2.setAttribute("style","display: block;");
+    timeLeft.innerHTML = secondsLeft;
     nextQuestion();
 })
 
@@ -137,13 +182,14 @@ function checkAnswer(event){
 
  function addScore(){
     doneSection.setAttribute("class", "sectionHide");
-    leaderboard.setAttribute("class", "sectionShow")
+    leaderboard.setAttribute("class", "sectionShow");
     scores.push({'initials':initialsInput.value,'score':score});
     localStorage.setItem("scores", JSON.stringify(scores));
     scores.sort( (a,b) => b.score - a.score );
     // i got the above line from https://devsheet.com/sort-array-of-objects-by-key-value-in-javascript/
     for( var i = 0; i < scores.length; i++){
         var tag1 = document.createElement("h2");
+        tag1.setAttribute("class", "scoreH2");
         var tag2 = document.createElement("span");
         tag1.textContent = scores[i].initials+ ": ";
         tag2.textContent = scores[i].score;
@@ -151,8 +197,38 @@ function checkAnswer(event){
         tag1.appendChild(tag2);
 
     }
+}
 
-   
-   
+clearBtn.addEventListener("click", clearScores);
+restartBtn.addEventListener("click", restartQuiz);
 
- }
+function restartQuiz(){
+    leaderboard.setAttribute("class", "sectionHide");
+    introSection.setAttribute("class", "sectionShow");
+    secondsLeft = 60;
+    timeLeft.innerHTML = secondsLeft;
+    timerH2.setAttribute("style","display: hidden;");
+    score = 0;
+    initialsInput.value = null;
+    questionIndex = 0;
+    scores = JSON.parse(localStorage.getItem("scores")) ;
+    if (scores == null){
+        scores = [];
+        localStorage.setItem("scores", JSON.stringify(scores))
+    }
+
+}
+
+function clearScores(){
+    localStorage.clear();
+    var tags = document.querySelectorAll(".scoreH2");
+    console.log(tags);
+    for(var i = 0; i < tags.length; i++){
+        tags[i].remove();
+    }
+    scores = JSON.parse(localStorage.getItem("scores")) ;
+    if (scores == null){
+        scores = [];
+        localStorage.setItem("scores", JSON.stringify(scores))
+    }
+}
